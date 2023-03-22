6a314bb4d30090702d5bff5a0ae654be4da46d6b
alex <alexandraagius@gmail.com>
2023-03-22 17:49:40 -0400

    adding index

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import './custom.scss';
+import Brinewater from './img/stages_splat3/Brinewater_Springs.png';
 
  function component() {
    const element = document.createElement('div');
@@ -6,7 +8,19 @@ import _ from 'lodash';
   // Lodash, now imported by this script
    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+   const Brinewater = new Image();
+      Brinewater.src = Brinewater;
+
+      element.appendChild(Brinewater);
+
    return element;
  }
 
  document.body.appendChild(component());
+
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+
+
