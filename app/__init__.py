edda6f2aa9f38bae684222dc9a0310b08559c959
alex <alexandraagius@gmail.com>
2023-03-28 15:41:54 -0400

    adding index

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+
+app = Flask(__name__,
+    template_folder='templates',
+    static_folder='templates/src'
+            )
+
+
 from app import views
